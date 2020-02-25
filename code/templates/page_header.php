beaaf4aa25d44b2bbb3a31e6f451beaa37fbb593
SpencerLee <spacecar@localhost.localdomain>
2020-02-25 16:55:42 -0500

    Added error message layout in this file, to be referenced whenever an error/warning is fired from the login/google-authenticator or any other relevent page to supress PHP error messages

--- code/templates/page_header.php
+++ code/templates/page_header.php
@@ -2,4 +2,15 @@
 	session_start();
 	include("config.php");
 	include("lib/db.php");
+
+#added
+function error($x){
+	if ($x == 1){
+		throw new Exception("Invalid Syntax used");
+	}else if ($x == 2){
+		throw new Exception("Wrong username/password");
+	}else if ($x == 3){
+		throw new Exception(" - Wrong QR code");
+	}
+}
 ?>
