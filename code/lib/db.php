68339649760423fb8318bdc490ebb8a4b893d4af
SpencerLee <spacecar@localhost.localdomain>
2020-02-25 16:51:59 -0500

    Created new method 'run_query_prepared()' which is called by any query with user input, additionly htmlspecialchars() and preg_match() methods were added to verify the vadility of the any user input

--- code/lib/db.php
+++ code/lib/db.php
@@ -1,5 +1,4 @@
 <?php
-
 $dbconn = pg_pconnect("host=$pg_host port=$pg_port dbname=$pg_dbname user=$pg_dbuser password=$pg_dbpassword") or die("Could not connect");
 if ($debug) {
 	echo "host=$pg_host, port=$pg_port, dbname=$pg_dbname, user=$pg_dbuser, password=$pg_dbpassword<br>";
@@ -22,10 +21,25 @@ function run_query($dbconn, $query) {
 	return $result;
 }
 
+#My function 
+function run_query_prepared($dbconn, $query_name, $array) {
+	if ($debug) {
+		echo "$query_name<br>";
+	}
+	$result = pg_execute($dbconn, $query_name, $array);
+	pg_query($dbconn, "deallocate " . $query_name);
+	pg_close($dbconn);
+	if ($result == False and $debug) {
+		echo "Query failed<br>";
+	}
+	return $result;	
+}
+
+
 //database functions
 function get_article_list($dbconn){
-	$query= 
-		"SELECT 
+	# Dont need prepared statment for this function 
+	$query = "SELECT 
 		articles.created_on as date,
 		articles.aid as aid,
 		articles.title as title,
@@ -37,12 +51,11 @@ function get_article_list($dbconn){
 		authors ON articles.author=authors.id
 		ORDER BY
 		date DESC";
-return run_query($dbconn, $query);
+	return run_query($dbconn, $query);
 }
 
 function get_article($dbconn, $aid) {
-	$query= 
-		"SELECT 
+	pg_prepare($dbconn, "get_article", "SELECT 
 		articles.created_on as date,
 		articles.aid as aid,
 		articles.title as title,
@@ -54,53 +67,48 @@ function get_article($dbconn, $aid) {
 		INNER JOIN
 		authors ON articles.author=authors.id
 		WHERE
-		aid='".$aid."'
-		LIMIT 1";
-return run_query($dbconn, $query);
+		aid= $1
+		LIMIT 1");
+	return run_query_prepared($dbconn, "get_article", array(htmlspecialchars($aid)));
 }
 
 function delete_article($dbconn, $aid) {
-	$query= "DELETE FROM articles WHERE aid='".$aid."'";
-	return run_query($dbconn, $query);
+	pg_prepare($dbconn, "delete_article", "DELETE FROM articles WHERE aid= $1 ");
+	return run_query_prepared($dbconn, "delete_article", array(htmlspecialchars($aid)));
 }
 
 function add_article($dbconn, $title, $content, $author) {
 	$stub = substr($content, 0, 30);
 	$aid = str_replace(" ", "-", strtolower($title));
-	$query="
+	pg_prepare($dbconn, "add_article", "
 		INSERT INTO
 		articles
 		(aid, title, author, stub, content) 
 		VALUES
-		('$aid', '$title', $author, '$stub', '$content')";
-	return run_query($dbconn, $query);
+		($1 , $2 , $3 , $4, $5)");
+	return run_query_prepared($dbconn, "add_article", array($aid, $title, $author, $stub, $content));
 }
 
 function update_article($dbconn, $title, $content, $aid) {
-	$query=
-		"UPDATE articles
-		SET 
-		title='$title',
-		content='$content'
-		WHERE
-		aid='$aid'";
-	return run_query($dbconn, $query);
+	pg_prepare($dbconn, "add_article", "UPDATE articles SET title= $1, content= $2 WHERE aid= $3"); 
+	return run_query_prepared($dbconn, "add_article", array(htmlspecialchars($title), htmlspecialchars($content), htmlspecialchars($aid)));
 }
 
 function authenticate_user($dbconn, $username, $password) {
-	$query=
-		"SELECT
-		authors.id as id,
-		authors.username as username,
-		authors.password as password,
-		authors.role as role
-		FROM
-		authors
-		WHERE
-		username='".$_POST['username']."'
-		AND
-		password='".$_POST['password']."'
-		LIMIT 1";
-	return run_query($dbconn, $query);
+	# Also used prepated statment to mitigate sql injections
+	if(filter($username) && filter($password)){
+		pg_prepare($dbconn, "auth", "SELECT authors.id as id, authors.username as username, authors.password as password, authors.role as role FROM authors WHERE username= $1 AND password= $2 LIMIT 1");
+		return run_query_prepared($dbconn, "auth", array($username, $password));
+	}
+    return "banana";
 }	
+
+function filter($input){
+	# Created an 'if' statment using htmlspecialchars() and regex match to catch any special charcters not caught by htmlspecialchars()
+	if(!preg_match('/^(["\'\;#]).*\1$/m', $input) && htmlspecialchars($input, ENT_QUOTES) == $input){
+		return true;	
+	}else{
+		return false;
+	}
+}
 ?>
