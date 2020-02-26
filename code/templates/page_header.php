ac143473ca567c7a2241481b420f40e5890b9267
SpencerLee <spacecar@spencer.fedora>
2020-02-26 02:10:01 -0500

    custom error messages

--- code/templates/page_header.php
+++ code/templates/page_header.php
@@ -3,14 +3,14 @@
 	include("config.php");
 	include("lib/db.php");
 
-#added
+#Custom error messages determined by $x value
 function error($x){
 	if ($x == 1){
 		throw new Exception("Invalid Syntax used");
 	}else if ($x == 2){
 		throw new Exception("Wrong username/password");
 	}else if ($x == 3){
-		throw new Exception(" - Wrong QR code");
+		throw new Exception("Invalid Permissions");
 	}
 }
 ?>
