3dd188c999974a7c2f4d2eb1cb6b728c9a5c233b
SpencerLee <spacecar@spencer.fedora>
2020-02-26 02:15:46 -0500

    Added comments for proof of authentication for username/password and google gr-coed

--- code/lib/auth.php
+++ code/lib/auth.php
@@ -1,7 +1,9 @@
 <?php
+// redirect the user to login.php if they arent authenticated via username/password
 if (!isset($_SESSION['authenticated'])) {
 	Header ("Location: /login.php");
 }
+// redirect the user google-authenticator.php page if they arent authenticated via qr-code
 if (!isset($_SESSION['authenticatedOTP']) && isset($_SESSION['authenticated'])) {
 	Header ("Location: /google-authenticator.php");
 }
